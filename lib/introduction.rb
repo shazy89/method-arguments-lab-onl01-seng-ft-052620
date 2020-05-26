8d2eda40e09df6b497f13cced85619e877e39578
Ed Shaziman <erdogan_oh@hotmail.com>
2020-05-26 14:23:06 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,7 @@
 def introduction (name)
   puts "Hi, my name is #{name}"
 end 
-introduction ('Josh')
+
 
 def introduction_with_language (x,y)
   puts "Hi, my name is#{x} and I am learning to program in #{y}."
